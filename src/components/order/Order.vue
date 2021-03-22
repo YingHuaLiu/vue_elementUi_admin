cbdc3f1fcc0156cf3024fd215c5fad38fb1044cd
YingHuaLiu <1042413535@qq.com>
2021-03-22 17:39:53 +0800

    finish report

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -40,7 +40,7 @@
             {{ scope.row.create_time | dateFormat }}
           </template>
         </el-table-column>
-        <el-table-column label="操作" v-slot="scope">
+        <el-table-column label="操作">
           <el-button size="mini" type="primary" icon="el-icon-edit" @click="showBox"></el-button>
           <el-button size="mini" type="success" icon="el-icon-location" @click="showProgressBox"></el-button>
         </el-table-column>
