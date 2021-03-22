32038030f221a9fdea23f99e8aff2f343ccdef30
YingHuaLiu <1042413535@qq.com>
2021-03-22 16:47:41 +0800

    finish order

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -6,7 +6,11 @@ import {
   Tag, Tree, Select, Option, Cascader, Alert, TabPane, Tabs, Steps, Step,
   CheckboxGroup, Checkbox, Upload
 } from 'element-ui';
+import Timeline from './timeline/index';
+import TimelineItem from './timeline-item/index';
 
+Vue.use(TimelineItem);
+Vue.use(Timeline);
 Vue.use(Upload);
 Vue.use(Checkbox);
 Vue.use(CheckboxGroup);
